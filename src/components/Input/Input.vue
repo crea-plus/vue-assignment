b4aa3305506f29260ad5caeaef0570dbfb2599d3
Klemen Komel <klemen.komel@razum.si>
2024-02-12 21:49:11 +0100

    created global input component

---
+++ src/components/Input/Input.vue
@@ -0,0 +1,10 @@
+<template>
+  <input
+    v-model="model"
+    class="max-w-full px-5 py-3 text-lg rounded-xl border border-gray-300 bg-gray-200 focus:bg-gray-50 focus:outline-none focus:border-green-500"
+  />
+</template>
+
+<script setup lang="ts">
+const model = defineModel()
+</script>
